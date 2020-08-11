9433bf1a553a03483e09b534fa7b32740b190f31
Khalimudin <khalimudin@gmail.com>
2020-08-11 23:30:42 +0700

    product per category chart

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,6 +9,8 @@ function HomePage() {
   const firebase = useContext(FirebaseContext);
   const [categoriesCount, setCategoriesCount] = useState(0);
   const [productsCount, setProductsCount] = useState(0);
+  const [productCountPerCategory, setProductCountPerCategory] = useState({});
+  const [categoryMapper, setCategoryMapper] = useState({});
 
   const dataCount = [
     {
@@ -26,11 +28,26 @@ function HomePage() {
       .categories()
       .onSnapshot((querySnapshot) => {
         setCategoriesCount(querySnapshot.size);
+        const mapper = {};
+        querySnapshot.forEach((doc) => {
+          mapper[doc.id] = doc.data().name;
+        });
+        setCategoryMapper(mapper);
       });
 
     const unsubscribeProducts = firebase
       .products()
       .onSnapshot((querySnapshot) => {
+        const products = {};
+        querySnapshot.forEach((doc) => {
+          const categoryId = doc.data().category;
+          if (categoryId in Object.keys(products)) {
+            products[categoryId]++;
+          } else {
+            products[categoryId] = 1;
+          }
+          setProductCountPerCategory(products);
+        });
         setProductsCount(querySnapshot.size);
       });
 
@@ -39,6 +56,13 @@ function HomePage() {
       unsubscribeProducts();
     };
   }, [firebase]);
+
+  const dataProductPerCategory = Object.keys(
+    productCountPerCategory
+  ).map((catId) => ({
+    category: categoryMapper[catId],
+    count: productCountPerCategory[catId],
+  }));
 
   return (
     <ProtectedComponent>
@@ -51,6 +75,20 @@ function HomePage() {
             <BarChart width={480} height={250} data={dataCount}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="name" />
+              <YAxis
+                allowDecimals={false}
+                label={{ value: "Counts", angle: -90 }}
+              />
+              <Tooltip />
+              <Bar dataKey="count" fill="#40a9ff" />
+            </BarChart>
+          </Card>
+
+          <Card style={{ maxWidth: 600 }}>
+            <Typography.Title level={3}>Products per Category</Typography.Title>
+            <BarChart width={480} height={250} data={dataProductPerCategory}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="category" />
               <YAxis
                 allowDecimals={false}
                 label={{ value: "Counts", angle: -90 }}
