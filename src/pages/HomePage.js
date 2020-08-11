4ff54d217fd0efcf756d3c4de5c74d782b53647a
Khalimudin <khalimudin@gmail.com>
2020-08-11 12:12:46 +0700

    logout functionality

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import FirebaseContext from "../contexts/firebase";
+import * as ROUTES from "../constants/routes";
 
 function HomePage() {
-  return <div>Home Page</div>;
+  const history = useHistory();
+  const firebase = useContext(FirebaseContext);
+  const [error, setError] = useState("");
+
+  const logout = () => {
+    firebase
+      .doSignOut()
+      .then(() => {
+        history.push(ROUTES.SIGN_IN);
+      })
+      .catch((error) => {
+        setError("Error happened when try to logout");
+      });
+  };
+  return (
+    <div>
+      Home Page
+      <button onClick={logout}>Logout</button>
+      {error && <p>{error.message}</p>}
+    </div>
+  );
 }
 
 export default HomePage;
